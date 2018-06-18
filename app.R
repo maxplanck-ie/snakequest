7f82a722e42c72abe0292d02f1b4bca6115844e3
Katarzyna Sikora <sikora@pc329.immunbio.mpg.de>
2018-06-18 17:53:26 +0200

    risky changes

--- app.R
+++ app.R
@@ -5,6 +5,8 @@ library(shiny,lib.loc=Rlib)
 library(shinydashboard,lib.loc=Rlib)
 library(rhandsontable,lib.loc=Rlib)
 
+
+
 ui <- function(request) {dashboardPage(
     dashboardHeader(title = "Dataset selection"),
     ## Sidebar content
@@ -17,6 +19,7 @@ ui <- function(request) {dashboardPage(
         actionButton(inputId="adddataset", label="Add dataset"),
         selectInput(inputId="selectworkflow",label="Select NGS workflow",choices=c("ATAC-seq","ChIP-seq","DNA-mapping","HiC","RNA-seq","WGBS")),
         textInput(inputId="analysistitle", label="Analysis title", value = "", width = NULL, placeholder = NULL),
+        uiOutput("configurator"),
         imageOutput("logo"),
         tags$footer("Copyright 2018 MPI-IE Freiburg Bioinfo Core Unit")
         ),
@@ -87,7 +90,7 @@ server <- function(input, output, session) {
                     values$datshort<-gsub("_R1.fastq.gz","",basename(grep("*_R1.fastq.gz",values$datPath,value=TRUE)))}
             else{values$ispaired<-FALSE
                  output$ispaired<-renderText("Single reads detected.")
-                 values$Read1<-grep("*_R1.fastq.gz",values$datPath,value=TRUE)
+                 values$Read1<-grep("*.fastq.gz",values$datPath,value=TRUE)
                  output$Read1<-renderText(values$Read1)
                  values$datshort<-gsub(".fastq.gz","",basename(values$datPath))}
             if(length(unique(values$datshort))<length(values$datshort)){
@@ -98,75 +101,8 @@ server <- function(input, output, session) {
 
             ###############initiate reactive table to collect sample information ###############
             ###use 1 observer to update sample names from added data and onother one to update with manually typed user information
-        
-        observe({
-              input$adddataset
-              values$DF<-data.frame(SampleID=values$datshort,Group=(rep("NA",(length(values$datshort)))),Read1=values$Read1,ChIPgroup=factor(rep("NA",(length(values$datshort))),levels=c("ChIP","Input"),ordered=TRUE),MatchedInput=factor(rep("NA",(length(values$datshort))),levels=unique(values$datshort),ordered=TRUE),MarkWidth=factor(rep("NA",(length(values$datshort))),levels=c("Broad","Narrow"),ordered=TRUE),stringsAsFactors = F)
-        })
-            
-        observe({
-          if(!is.null(input$hot))
-            values$DF <- hot_to_r(input$hot)
-        })
-        
-        output$hot <- renderRHandsontable({
-          rhandsontable(values$DF, stretchH = "all")
-        })
 
-        
-      observeEvent(input$savetable, {
-          values$analysisName<-gsub("[^[:alnum:]]", "_", isolate(input$analysistitle))
-          values$ranstring<-stri_rand_strings(n=1,length=8)
-          
-          sampleInfo<-isolate(values$DF)
-          sampleInfo<-sampleInfo[!sampleInfo$Group %in% "NA",]
-          rownames(sampleInfo)<-sampleInfo$SampleID
-          
-          ###check for replicates, else issue a warning
-          gl<-min(ave(1:nrow(sampleInfo),sampleInfo$Group,FUN=length))
-          if(gl==1){
-            values$datwarnings<-c(values$datwarnings,"SOME SAMPLE GROUPS DON'T HAVE REPLICATES!!!")
-          }
-          else if (gl==2){
-            values$datwarnings<-c(values$datwarnings,"Some of your sample groups have only 2 replicates. This might be suboptimal for some analyses and lead to higher FDR.")
-          }
-          else if (gl==3){
-            values$datwarnings<-c(values$datwarnings,"All sample groups have at least 3 replicates.")
-          }
-          
-          ###check if ChiPGroup is filled, if yes, create a yaml
-          if(sum(is.na(sampleInfo$ChIPgroup)<1)){
-          chip_dict<-sInfoTOyaml(sampleInfo)
-          values$chDictDest<-sprintf("/data/manke/group/shiny/snakepipes_input/%s_%s_samples.yaml",values$ranstring,values$analysisName)
-          write_yaml(noquote(chip_dict),file=isolate(values$chDictDest))
-          
-          }
-          
-          #update read1 and read2 stored in reactive values
-          if (values$ispaired){
-            vRead1<-sampleInfo$Read1
-            vRead2<-gsub("_R1.fastq.gz","_R2.fastq.gz",sampleInfo$Read1)
-            values$Reads<-c(vRead1,vRead2)
-          } else {
-            vRead1<-sampleInfo$Read1
-            values$Reads<-vRead1
-          }
-          
-          
-          values$sInfoDest<-sprintf("/data/manke/group/shiny/snakepipes_input/%s_%s_sampleSheet.tsv",values$ranstring,values$analysisName)
-          
-          write.table(sampleInfo,file=values$sInfoDest,sep="\t",quote=FALSE)
-          output$sIsaved<-renderText("Sample sheet saved.")
-          
-                  })#end of observe input$savetable
-      
-      output$configurator<-renderUI({tagList(
-        selectInput(inputId="genome", label="Select organism", choices=c("Zebrafish","Fission yeast","Fruitfly","Human","Mouse"), selected = NULL),
-        selectInput(inputId="version", label="Select workflow version", choices=c("Placeholder"), selected = "Placeholder")
-      )})
-       
-       
-   observe({input$selectworkflow
+        observe({input$selectworkflow
            values$inWorkflow<-input$selectworkflow
            values$inGroup<-input$group
            
@@ -231,12 +167,96 @@ server <- function(input, output, session) {
              values$command<-sprintf("mkdir -p %s ; %s ; %s ; %s -ri %s -w %s --sampleInfo %s %s ",indir,link_cmd,cp_sInfo_cmd,path_to_exec,indir,outdir,values$sInfo_in,genome_sel[input$genome]) 
              output$command<-renderText({ values$command })
              
-           }
-           
+           } #end of WGBS
 
-           
+
+        observe({
+              input$adddataset
+              input$selectworkflow
+              
+              if(values$inWorkflow=="ChIPseq"){
+              values$DF<-data.frame(SampleID=values$datshort,Group=(factor(rep("NA",(length(values$datshort))),levels=c("Control","Treatment"))),Read1=values$Read1,ChIPgroup=factor(rep("NA",(length(values$datshort))),levels=c("ChIP","Input"),ordered=TRUE),MatchedInput=factor(rep("NA",(length(values$datshort))),levels=unique(values$datshort),ordered=TRUE),MarkWidth=factor(rep("NA",(length(values$datshort))),levels=c("Broad","Narrow"),ordered=TRUE),stringsAsFactors = F)} 
+
+              if(values$inWorkflow=="HiC"){
+              values$DF<-data.frame(SampleID=values$datshort,Group=(rep("NA",(length(values$datshort)))),Read1=values$Read1,stringsAsFactors = F)}
+
+
+               else {
+
+               values$DF<-data.frame(SampleID=values$datshort,Group=(factor(rep("NA",(length(values$datshort))),levels=c("Control","Treatment"))),Read1=values$Read1,stringsAsFactors = F)
+               }
+
+        })
+
+
+        observe({
+          if(!is.null(input$hot))
+            values$DF <- hot_to_r(input$hot)
+        })
+        
+        output$hot <- renderRHandsontable({
+          rhandsontable(values$DF, stretchH = "all")
+        })
+
+ 
       })#end of observe input$selectworkflow 
      
+         
+      observeEvent(input$savetable, {
+          values$analysisName<-gsub("[^[:alnum:]]", "_", isolate(input$analysistitle))
+          values$ranstring<-stri_rand_strings(n=1,length=8)
+          
+          sampleInfo<-isolate(values$DF)
+          sampleInfo<-sampleInfo[!sampleInfo$Group %in% "NA",]
+          rownames(sampleInfo)<-sampleInfo$SampleID
+          sampleInfo$Group<-as.character(sampleInfo$Group)
+          sampleInfo<-sampleInfo[,sort(sampleInfo$Group)]
+          if(values$inWorkflow!="WGBS"){colnames(sampleInfo)[1:2]<-c("sample","condition")}
+          
+          ###check for replicates, else issue a warning
+          gl<-min(ave(1:nrow(sampleInfo),sampleInfo$Group,FUN=length))
+          if(gl==1){
+            values$datwarnings<-c(values$datwarnings,"SOME SAMPLE GROUPS DON'T HAVE REPLICATES!!!")
+          }
+          else if (gl==2){
+            values$datwarnings<-c(values$datwarnings,"Some of your sample groups have only 2 replicates. This might be suboptimal for some analyses and lead to higher FDR.")
+          }
+          else if (gl==3){
+            values$datwarnings<-c(values$datwarnings,"All sample groups have at least 3 replicates.")
+          }
+          
+          ###check if ChiPGroup is filled, if yes, create a yaml
+          if(values$inWorkflow=="ChIP-seq"){
+          chip_dict<-sInfoTOyaml(sampleInfo)
+          values$chDictDest<-sprintf("/data/manke/group/shiny/snakepipes_input/%s_%s_samples.yaml",values$ranstring,values$analysisName)
+          write_yaml(noquote(chip_dict),file=isolate(values$chDictDest))
+          
+          }
+          
+          #update read1 and read2 stored in reactive values
+          if (values$ispaired){
+            vRead1<-sampleInfo$Read1
+            vRead2<-gsub("_R1.fastq.gz","_R2.fastq.gz",sampleInfo$Read1)
+            values$Reads<-c(vRead1,vRead2)
+          } else {
+            vRead1<-sampleInfo$Read1
+            values$Reads<-vRead1
+          }
+          
+          
+          values$sInfoDest<-sprintf("/data/manke/group/shiny/snakepipes_input/%s_%s_sampleSheet.tsv",values$ranstring,values$analysisName)
+          
+          write.table(sampleInfo,file=values$sInfoDest,sep="\t",quote=FALSE)
+          output$sIsaved<-renderText("Sample sheet saved.")
+          
+                  })#end of observe input$savetable
+      
+      output$configurator<-renderUI({tagList(
+        selectInput(inputId="genome", label="Select organism", choices=c("Zebrafish","Fission yeast","Fruitfly","Human","Mouse"), selected = NULL)
+      )})
+       
+       
+   
              observeEvent(input$savesubmit, {
                bshscript<-sprintf("/data/manke/group/shiny/snakepipes_input/%s_%s_script.sh",values$ranstring,values$analysisName)
                fileConn<-file(bshscript)
@@ -268,8 +288,6 @@ server <- function(input, output, session) {
         output$resultPanels<-renderUI({myTabs<-list(tabPanel(title="Input Data",
                                                       fluidPage(
                                                           box(textOutput("ispaired"),width=12),
-                                                          box(textOutput("Read1"),title="Read1",width=12,collapsible=TRUE),
-                                                          #fluidRow(box(textOutput("Read2"),title="Read2")),
                                                           rHandsontableOutput("hot"),
                                                           box(textOutput("datawarnings")),
                                                               actionButton(inputId="savetable",label="Save sample sheet",style="background-color: green"),
@@ -278,12 +296,10 @@ server <- function(input, output, session) {
                                                               )
                                                           ),
                                                     
-                                                    tabPanel(title="Workflow parameters",
+                                                    tabPanel(title="User information",
                                                              fluidPage(
                                                                fluidRow(
-                                                                 uiOutput("configurator"),
                                                                  uiOutput("from"),
-                                                                 box(textOutput("command")),
                                                                  uiOutput("freetext")
                                                                        ),
                                                                  actionButton(inputId="savesubmit",label="Save workflow configuration and submit request"),
