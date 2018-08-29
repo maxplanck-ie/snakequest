abec9f68ae3f496ab4982679401c8e57d84c58f7
Katarzyna Sikora <sikora@pc329.immunbio.mpg.de>
2018-08-29 15:05:14 +0200

    added bam input to WGBS

--- app.R
+++ app.R
@@ -1,5 +1,5 @@
 ## app.R ##
-Rlib="/data/boehm/group/shiny_apps/Rlibs3.4.0"
+Rlib="/data/boehm/group/shiny_apps/Rlibs3.5.0"
 
 library(shiny,lib.loc=Rlib)
 library(shinydashboard,lib.loc=Rlib)
@@ -15,6 +15,7 @@ ui <- function(request) {dashboardPage(
         selectInput(inputId="selectworkflow",label="Select NGS workflow",choices=c("PLEASE SELECT WORKFLOW","ATAC-seq","ChIP-seq","DNA-mapping","HiC","RNA-seq","WGBS")),
         textInput(inputId="analysistitle", label="Analysis title", value = "", width = NULL, placeholder = NULL),
         selectInput(inputId="genome", label="Select organism", choices=c("PLEASE SELECT A GENOME","Zebrafish [zv10]","Fission yeast","Fruitfly [dm6]","Fruitfly [dm3]","Human [hg37]","Human [hg38]","Mouse [mm9]","Mouse [mm10]"), selected = NULL),
+        selectInput(inputId="selectformat",label="Select input file format",choices=c("fastq.gz","bam")),
         textInput(inputId="group", label="Group", value = "", width = NULL, placeholder = NULL),
         textInput(inputId="owner", label="Project Owner", value = "", width = NULL, placeholder = NULL),
         textInput(inputId="projectid", label="Project ID", value = "", width = NULL, placeholder = NULL),
@@ -72,37 +73,51 @@ server <- function(input, output, session) {
         
         
         observeEvent(input$adddataset, {
-          
+        
+        dsel<-c("fastq.gz"="Project","bam"="Analysis")
+        psel<-c("fastq.gz"="*fastq.gz$","bam"="*.bam$") 
+        inFormat<-isolate(input$selectformat)
         
         if((input$group!="")&(input$owner!="")&(input$projectid!="")&(input$pathtodata=="")){
             inGroup<-isolate(input$group)
             inOwner<-isolate(input$owner)
             inProjectID<-isolate(input$projectid)
+            
+            
+            
+            
 
-            values$datdir<-c(values$datdir,system(sprintf("find /data/%s/sequencing_data -name Project_%s_%s_%s -type d | sort",tolower(inGroup),inProjectID,inOwner,inGroup),intern=TRUE)) 
+            values$datdir<-c(values$datdir,system(sprintf("find /data/%s/sequencing_data -name %s_%s_%s_%s -type d | sort",tolower(inGroup),dsel[inFormat],inProjectID,inOwner,inGroup),intern=TRUE)) 
         }
             
         else if ((input$group=="")&(input$owner=="")&(input$projectid=="")&(input$pathtodata!="")){
             values$datdir<-c(values$datdir,isolate(input$pathtodata))
                 }  
             #handle single and paired end data ###allow for multiple sequencing runs
-            values$datPath<-dir(values$datdir,pattern="*.fastq.gz",full.names=TRUE,recursive=TRUE)
-            #get rid of optical duplicates
-            values$datPath<-grep("*optical*",values$datPath,value=TRUE,invert=TRUE)
-                                           
-        
-            if(sum(grepl(".+_R*2.fastq.gz",values$datPath,perl=TRUE))>0){values$ispaired<-TRUE
-                    output$ispaired<-renderText("Paired reads detected.")
-                    values$Read1<-grep(".+_R*1.fastq.gz",values$datPath,value=TRUE,perl=TRUE)
-                    values$Read2<-grep(".+_R*2.fastq.gz",values$datPath,value=TRUE,perl=TRUE)
-                    output$Read1<-renderText(values$Read1)
-                    output$Read2<-renderText(values$Read2)
-                    values$datshort<-gsub("_R*1.fastq.gz","",basename(grep(".+_R*1.fastq.gz",values$datPath,value=TRUE,perl=TRUE)),perl=TRUE)}
-            else{values$ispaired<-FALSE
-                 output$ispaired<-renderText("Single reads detected.")
-                 values$Read1<-grep("*.fastq.gz",values$datPath,value=TRUE)
-                 output$Read1<-renderText(values$Read1)
-                 values$datshort<-gsub(".fastq.gz","",basename(values$datPath))}
+            values$datPath<-dir(values$datdir,pattern=psel[inFormat],full.names=TRUE,recursive=TRUE)
+            
+            
+            if(inFormat %in% "bam"){
+              values$ispaired<-FALSE
+              values$Read1<-grep(".+.bam$",values$datPath,value=TRUE,perl=TRUE)
+              values$datshort<-gsub(".bam","",basename(values$datPath))                             
+            }
+            else{
+                #get rid of optical duplicates
+                values$datPath<-grep("*optical*",values$datPath,value=TRUE,invert=TRUE)
+                if(sum(grepl(".+_R*2.fastq.gz",values$datPath,perl=TRUE))>0){values$ispaired<-TRUE
+                        output$ispaired<-renderText("Paired reads detected.")
+                        values$Read1<-grep(".+_R*1.fastq.gz",values$datPath,value=TRUE,perl=TRUE)
+                        values$Read2<-grep(".+_R*2.fastq.gz",values$datPath,value=TRUE,perl=TRUE)
+                        output$Read1<-renderText(values$Read1)
+                        output$Read2<-renderText(values$Read2)
+                        values$datshort<-gsub("_R*1.fastq.gz","",basename(grep(".+_R*1.fastq.gz",values$datPath,value=TRUE,perl=TRUE)),perl=TRUE)}
+                else{values$ispaired<-FALSE
+                     output$ispaired<-renderText("Single reads detected.")
+                     values$Read1<-grep("*.fastq.gz",values$datPath,value=TRUE)
+                     output$Read1<-renderText(values$Read1)
+                     values$datshort<-gsub(".fastq.gz","",basename(values$datPath))}
+            }
             if(length(unique(values$datshort))<length(values$datshort)){
                 values$datwarnings<-"Warning! Your dataset contains multiple instances of identically named samples! If these are resequenced samples, consider merging them before continuing with the analysis or exclude them by leaving NAs in the Group column. Otherwise they will be overwritten."
             }else{values$datwarnings<-"All sample names are unique. Proceed with the analysis."}
@@ -117,7 +132,8 @@ server <- function(input, output, session) {
            
            path_to_exec<-paste0("module load snakePipes; ",values$inWorkflow)###add version selection
            topdir<-sprintf("/data/processing/bioinfo-core/requests/%s_%s_%s",values$analysisName,values$ranstring,values$inWorkflow)
-           indir<-sprintf("%s/fastq",topdir)
+           dsel<-c("fastq.gz"="fastq","bam"="bam")
+           indir<-sprintf("%s/%s",topdir,dsel[input$selectformat])
            link_cmd<- paste0("ln -t ",indir,' -s ',paste(values$Reads,collapse=" "))
            
            outdir<-sprintf("%s/analysis",topdir)
@@ -131,6 +147,7 @@ server <- function(input, output, session) {
           
            path_to_DNA_mapping<-paste0("module load snakePipes; DNA-mapping")
            
+           fbam<-c("fastq.gz"="","bam"="--fromBam")
            
            if(values$inWorkflow=="ATAC-seq"){
                
@@ -175,7 +192,7 @@ server <- function(input, output, session) {
            
            else if(values$inWorkflow=="WGBS"){
              
-             values$command<-sprintf("mkdir -p %s ; %s ; %s ; %s -i %s -o %s --sampleInfo %s %s ",indir,link_cmd,cp_sInfo_cmd,path_to_exec,indir,outdir,values$sInfo_in,values$genome) 
+             values$command<-sprintf("mkdir -p %s ; %s ; %s ; %s -i %s -o %s --sampleInfo %s %s %s ",indir,link_cmd,cp_sInfo_cmd,path_to_exec,indir,outdir,values$sInfo_in,fbam[input$selectformat],values$genome) 
              output$command<-renderText({ values$command })
              
            } #end of WGBS
@@ -275,6 +292,7 @@ server <- function(input, output, session) {
       
       
       output$datarequests<- renderUI({tagList(
+        checkboxInput(inputId="fbam", label="I want to start the analysis from the bam files.", value = FALSE, width = NULL),
         checkboxInput(inputId="merge", label="I want to request sample merging.", value = FALSE, width = NULL),
         checkboxInput(inputId="beff", label="I expect batch effect in my data.", value = FALSE, width = NULL),
         checkboxInput(inputId="nodiff", label="I don't need differential analysis.", value = FALSE, width = NULL)
@@ -288,6 +306,7 @@ server <- function(input, output, session) {
                exports<-""
                writeLines(c(shebang,exports,unlist(strsplit(isolate(values$command),split=";"))), fileConn)
                close(fileConn)
+               fbam_request<-ifelse(isolate(input$fbam),"I want to start the analysis from the bam files.","I want to start the analysis from fastq files.")
                merge_request<-ifelse(isolate(input$merge),"I want to request sample merging. Please consider the information I entered in the Merge column of the sample sheet. \n Please update the sample sheet after merging files.","No sample merging is needed.")
                b_eff_request<-ifelse(isolate(input$beff),"I expect batch effect in my data.","No batch effect is expected.")
                nodiff_request<-ifelse(isolate(input$nodiff),"I don't need differential analysis.","I want to request differential analysis.")
@@ -295,8 +314,9 @@ server <- function(input, output, session) {
                #from<-sprintf("<sendmailR@%s>", Sys.info()[4])
                from<-"sendmailR@ie-freiburg.mpg.de"
                to<-"bioinfo-core@ie-freiburg.mpg.de" 
+               #to<-"sikora@ie-freiburg.mpg.de"
                subject<-paste0("Analysis request ",isolate(input$analysistitle), "_" ,isolate(values$ranstring))
-               msg <- gsub(";","\n \n",paste0(cc," has requested the following analysis: \n \n Workflow: ", isolate(values$inWorkflow)," \n \n Genome: ", values$genome," \n \n ", merge_request," \n \n ", b_eff_request ," \n \n ", nodiff_request," \n \n User comments: \n \n", isolate(input$comments),"\n \n Please review the input files and the attached sample sheet before proceeding. \n \n End of message. \n \n ",paste(rep("#",times=80),collapse="")," \n \n ", values$command ,"\n \n"))
+               msg <- gsub(";","\n \n",paste0(cc," has requested the following analysis: \n \n Workflow: ", isolate(values$inWorkflow)," \n \n Genome: ", values$genome," \n \n ", fbam_request, " \n \n " ,merge_request," \n \n ", b_eff_request ," \n \n ", nodiff_request," \n \n User comments: \n \n", isolate(input$comments),"\n \n Please review the input files and the attached sample sheet before proceeding. \n \n End of message. \n \n ",paste(rep("#",times=80),collapse="")," \n \n ", values$command ,"\n \n"))
                if(values$inWorkflow=="ChIP-seq"){
                  sendmail(from=sprintf("%s",from), to=to, subject=subject, control=list(smtpServer="owa.ie-freiburg.mpg.de"), msg=list(msg,mime_part(isolate(values$sInfoDest)),mime_part(isolate(values$chDictDest)),mime_part(bshscript)),cc=sprintf("%s",cc))
                }
